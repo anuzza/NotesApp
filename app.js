13c7e8081be9eeedf2a10cd68732619817690c7f
anuzza <anuja3580@gmail.com>
2022-09-25 22:06:58 -0500

    argument passing with yargs

--- app.js
+++ app.js
@@ -1,11 +1,45 @@
 const chalk = require("chalk");
+const yargs = require("yargs");
 const getNotes = require("./notes.js");
 
-const command = process.argv;
-//console.log(command);
+//Customize yargs version
+yargs.version("1.1.0");
 
-if (command[2] === "add") {
-  console.log(command[4]);
-} else if (command === "remove") {
-  console.log("removing note");
-}
+//create add command
+yargs.command({
+  command: "add",
+  describe: "Add a new note",
+  handler: function () {
+    console.log("Adding new note");
+  },
+});
+
+//create remove command
+yargs.command({
+  command: "remove",
+  describe: "remove a note",
+  handler: function () {
+    console.log("removing a note");
+  },
+});
+
+//create list command
+yargs.command({
+  command: "list",
+  describe: "listing your notes",
+  handler: function () {
+    console.log("listing out all notes");
+  },
+});
+
+//create read command
+yargs.command({
+  command: "read",
+  describe: "read a note",
+  handler: function () {
+    console.log("reading a note");
+  },
+});
+
+//add, remove, read, list
+console.log(yargs.argv);
