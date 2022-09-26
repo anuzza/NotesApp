dd62893ebea8565b3db55b311969a82fed44f89e
anuzza <anuja3580@gmail.com>
2022-09-25 21:34:11 -0500

    printing in color

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
+const validator = require("validator");
+const chalk = require("chalk");
 const getNotes = require("./notes.js");
 console.log(getNotes());
+console.log(validator.isEmail("anuja@example.com"));
+console.log(validator.isURL("https://google.com"));
+console.log(chalk.green.bold.inverse("Success!"));
