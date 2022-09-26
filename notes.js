6c34d82151eafbe5bd813d0367f1f1fa1792d757
anuzza <anuja3580@gmail.com>
2022-09-25 23:43:14 -0500

    reading a note

--- notes.js
+++ notes.js
@@ -1,15 +1,13 @@
 const fs = require("fs");
 const chalk = require("chalk");
-const getNotes = () => {
-  return "Your notes...";
-};
 
 const addNote = (title, body) => {
   const notes = loadNotes();
 
-  const duplicateNotes = notes.filter((note) => note.title === title);
+  //   const duplicateNotes = notes.filter((note) => note.title === title);
+  const duplicateNote = notes.find((note) => note.title === title);
 
-  if (duplicateNotes.length === 0) {
+  if (!duplicateNote) {
     notes.push({
       title: title,
       body: body,
@@ -32,6 +30,24 @@ const removeNote = (title) => {
   }
 };
 
+const listNotes = () => {
+  const notes = loadNotes();
+  console.log(chalk.inverse("Your notes"));
+  notes.forEach((note) => {
+    console.log(note.title);
+  });
+};
+
+const readNote = (title) => {
+  const notes = loadNotes();
+  const note = notes.find((note) => note.title === title);
+  if (note) {
+    console.log(chalk.blue.inverse(note.title));
+    console.log(note.body);
+  } else {
+    console.log(chalk.red.inverse("No note found"));
+  }
+};
 const saveNotes = (notes) => {
   const dataJSON = JSON.stringify(notes);
   fs.writeFileSync("notes.json", dataJSON);
@@ -48,7 +64,8 @@ const loadNotes = () => {
 };
 
 module.exports = {
-  getNotes: getNotes,
   addNote: addNote,
   removeNote: removeNote,
+  listNotes: listNotes,
+  readNote: readNote,
 };
